8cd62e086e89f273f6773afcaf1cc9e1538b477f
ahmadfaraj4000-gif <ahmad.faraj4000@gmail.com>
2026-06-26 00:42:03 -0400

    Add files via upload

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -1,10 +1,13 @@
-import logoUrl from "../../../assets/logos/logo.png";
+import logoUrl from "../assets/logo.png";
 
 export function AdminNav() {
   return (
     <nav className="nav adminNav">
       <div className="navin adminNavInner">
-        <div className="brand"><img src={logoUrl} alt="The Anchor Collective logo" /><span>THE ANCHOR ADMIN</span></div>
+        <div className="brand">
+          <img src={logoUrl} alt="The Anchor Collective logo" />
+          <span>THE ANCHOR ADMIN</span>
+        </div>
       </div>
     </nav>
   );
